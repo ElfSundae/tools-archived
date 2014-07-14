f8d165b4434bb6775a1f841d7a6e1302b9564bab
Elf Sundae <elf.sundae@gmail.com>
2014-07-15 02:27:52 +0800

    build libogg and speex

---
+++ speex/build_libogg.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+
+#
+# Build libogg for iOS.
+#
+# Xcode with command line tool installed, iOS 6.1, iOS 7.1, OSX 10.9 SDK required.
+#
+# Elf Sundae, www.0x123.com
+# Jul 14, 2014
+#
+
+LIB="libogg"
+LIB_VERSION="1.3.2"
+LIB_DIR=${LIB}-${LIB_VERSION}
+DEVELOPER_ROOT=`xcode-select -print-path`
+CURRENT_PATH=`pwd`
+BUILD_PATH=${CURRENT_PATH}/build
+BIN_PATH=${BUILD_PATH}/${LIB_DIR}
+LIBFILES=""
+BUILD="x86_64-apple-darwin"
+
+ARCHS=("i386" "x86_64" "armv7" "armv7s" "arm64")
+# ARCHS=("i386")
+
+if [ ! -d "${LIB_DIR}" ]; then
+	file="${LIB_DIR}.tar.gz"
+	if [ ! -f "${file}" ]; then
+		set -x
+		curl -O http://downloads.xiph.org/releases/ogg/${file}
+		set +x
+	fi
+	tar jxf $file
+fi
+
+cd ${LIB_DIR}
+
+for ARCH in ${ARCHS[@]}
+do
+	if [ "${ARCH}" == "i386" ]; then
+		PLATFORM="iPhoneSimulator"
+		HOST="i386-apple-darwin"
+		SDK_VERSION="6.1"
+	elif [ "${ARCH}" == "x86_64" ]; then
+		PLATFORM="MacOSX"
+		HOST="x86_64-apple-darwin"
+		SDK_VERSION="10.9"
+	else
+		PLATFORM="iPhoneOS"
+		HOST="arm-apple-darwin"
+		SDK_VERSION="7.1"
+	fi
+
+	SDK=${DEVELOPER_ROOT}/Platforms/${PLATFORM}.platform/Developer/SDKs/${PLATFORM}${SDK_VERSION}.sdk
+	PREFIX=${BUILD_PATH}/${LIB}/${ARCH}
+
+	rm -rf "${PREFIX}"
+	mkdir -p "${PREFIX}"
+
+	export CFLAGS="-arch ${ARCH} -isysroot ${SDK}"
+	export CC="${DEVELOPER_ROOT}/usr/bin/gcc ${CFLAGS}"
+	export CXXFLAGS="$CFLAGS"
+	export LDFLAGS="$CFLAGS"
+	export LD=$CC
+	
+	./configure --prefix="${PREFIX}" --host=${HOST} --build=${BUILD} --disable-shared --enable-static
+	make clean
+	make && make install
+	
+	echo "=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+	libfile="${PREFIX}/lib/${LIB}.a"
+	if [ ! -f "${libfile}" ]; then
+		echo "${ARCH} Error."
+		exit -1
+	fi
+	
+	lipo -info "${libfile}"
+	echo "=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+	
+	LIBFILES="${libfile} ${LIBFILES}"
+done
+echo ""
+
+rm -rf "${BIN_PATH}"
+mkdir "${BIN_PATH}"
+# copy headers
+cp -r "${BUILD_PATH}/${LIB}/${ARCHS[0]}/include/" "${BIN_PATH}/include/"
+# create fat libraries
+mkdir "${BIN_PATH}/lib"
+lipo -create ${LIBFILES} -output "${BIN_PATH}/lib/${LIB}.a"
+# check architectures information
+lipo -info "${BIN_PATH}/lib/${LIB}.a"
+
+echo "=*= Done =*="
---
+++ speex/build_speex.sh
@@ -0,0 +1,106 @@
+#!/bin/sh
+
+#
+# Build Speex for iOS.
+#
+# Xcode with command line tool installed, iOS 6.1, iOS 7.1, OSX 10.9 SDK required.
+#
+# Elf Sundae, www.0x123.com
+# Jul 14, 2014
+#
+
+LIB="speex"
+LIB_VERSION="1.2rc1"
+OGG="libogg-1.3.2"
+LIB_DIR=${LIB}-${LIB_VERSION}
+DEVELOPER_ROOT=`xcode-select -print-path`
+CURRENT_PATH=`pwd`
+BUILD_PATH=${CURRENT_PATH}/build
+BIN_PATH=${BUILD_PATH}/${LIB_DIR}
+LIBFILES=""
+LIBFILES_DSP=""
+BUILD="x86_64-apple-darwin"
+
+ARCHS=("i386" "x86_64" "armv7" "armv7s" "arm64")
+# ARCHS=("i386")
+
+if [ ! -f "${BUILD_PATH}/${OGG}/lib/libogg.a" ]; then
+	sh ./build_libogg.sh
+fi
+
+if [ ! -d "${LIB_DIR}" ]; then
+	file="${LIB_DIR}.tar.gz"
+	if [ ! -f "${file}" ]; then
+		set -x
+		curl -O http://downloads.xiph.org/releases/speex/${file}
+		set +x
+	fi
+	tar jxf $file
+fi
+
+cd ${LIB_DIR}
+
+for ARCH in ${ARCHS[@]}
+do
+	if [ "${ARCH}" == "i386" ]; then
+		PLATFORM="iPhoneSimulator"
+		HOST="i386-apple-darwin"
+		SDK_VERSION="6.1"
+	elif [ "${ARCH}" == "x86_64" ]; then
+		PLATFORM="MacOSX"
+		HOST="x86_64-apple-darwin"
+		SDK_VERSION="10.9"
+	else
+		PLATFORM="iPhoneOS"
+		HOST="arm-apple-darwin"
+		SDK_VERSION="7.1"
+	fi
+
+	SDK=${DEVELOPER_ROOT}/Platforms/${PLATFORM}.platform/Developer/SDKs/${PLATFORM}${SDK_VERSION}.sdk
+	PREFIX=${BUILD_PATH}/${LIB}/${ARCH}
+
+	rm -rf "${PREFIX}"
+	mkdir -p "${PREFIX}"
+
+	export CFLAGS="-arch ${ARCH} -isysroot ${SDK}"
+	export CC="${DEVELOPER_ROOT}/usr/bin/gcc ${CFLAGS}"
+	export CXXFLAGS="$CFLAGS"
+	export LDFLAGS="$CFLAGS"
+	export LD=$CC
+
+	./configure --prefix="${PREFIX}" --host=${HOST} --build=${BUILD} \
+		--disable-shared --enable-static \
+			-disable-oggtest -enable-fixed-point -disable-float-api \
+				--with-ogg="${BUILD_PATH}/${OGG}"
+
+	make clean
+	make && make install
+
+	echo "=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+	libfile="${PREFIX}/lib/lib${LIB}.a"
+	if [ ! -f "${libfile}" ]; then
+		echo "${ARCH} Error."
+		exit -1
+	fi
+
+	lipo -info "${libfile}"
+	echo "=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+
+	LIBFILES="${libfile} ${LIBFILES}"
+	LIBFILES_DSP="${PREFIX}/lib/lib${LIB}dsp.a ${LIBFILES_DSP}"
+done
+echo ""
+
+rm -rf "${BIN_PATH}"
+mkdir "${BIN_PATH}"
+# copy headers
+cp -r "${BUILD_PATH}/${LIB}/${ARCHS[0]}/include/" "${BIN_PATH}/include/"
+# create fat libraries
+mkdir "${BIN_PATH}/lib"
+lipo -create ${LIBFILES} -output "${BIN_PATH}/lib/lib${LIB}.a"
+lipo -create ${LIBFILES_DSP} -output "${BIN_PATH}/lib/lib${LIB}dsp.a"
+# check architectures information
+lipo -info "${BIN_PATH}/lib/lib${LIB}.a"
+lipo -info "${BIN_PATH}/lib/lib${LIB}dsp.a"
+
+echo "=*= Done =*="
